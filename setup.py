68e8ed299efd6b29a3a1a98eab7b91feaaff6cab
Krazyman <narutoshippuden12_@hotmail.com>
2018-07-18 11:57:22 +1000

    this should work

--- setup.py
+++ setup.py
@@ -1,4 +1,30 @@
-from distutils.core import setup
-import py2exe
+from cx_Freeze import setup, Executable
+import os.path
 
-setup(console=['scheduling.py'])
+PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
+os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
+
+base = None    
+
+executables = [Executable("scheduling.py", base=base)]
+
+packages = ["idna", "operator", "tkinter", "funcs", "os", "PIL"]
+options = {
+    'build_exe': {
+        'include_files':[
+            os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tk86t.dll'),
+            os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tcl86t.dll'),
+         ],
+    },
+}
+
+setup(
+    name = "Scheduling Program",
+    options = options,
+    author = "Manwai Nguyen",
+    author_email = "omitted",
+    version = "1.0",
+    description = 'Micronesian Brokers Inc. scheduling program',
+    executables = executables
+)
