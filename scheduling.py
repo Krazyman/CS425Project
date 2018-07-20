a88cd8b73452ff20886fe5c37c3e22384eb93185
Krazyman <narutoshippuden12_@hotmail.com>
2018-07-20 11:25:16 +1000

    Win32GUI

--- scheduling.py
+++ scheduling.py
@@ -191,7 +191,7 @@ def gui():
     textbox = Text(root,font=("Helvetica",8),state=DISABLED, yscrollcommand=scrollbar.set, wrap=WORD)
     # This puts the text box on the left hand side
     textbox.config(width=100, height=50)
-    textbox.grid(row=2, column=0, columnspan=8, sticky=N+S+W+E)
+    textbox.grid(row=2, rowspan=1, column=0, columnspan=8, sticky=N+S+W+E)
 
     # Configure the scroll bar to stroll with the text box!
     scrollbar.config(command=textbox.yview)
